2f1fbffe5dc1d6cfbd811799c5f1e5f839f6393e
Anna Whitmore <33288034+annawhitm@users.noreply.github.com>
2018-04-23 18:46:38 +0100

    Update database_1.sql

--- database_1.sql
+++ database_1.sql
@@ -1,50 +1,50 @@
 CREATE TABLE Customer (
     customerID   Integer    NOT NULL,
-    customerName Char(25)   NOT NULL,
+    customername Char(25)   NOT NULL,
     CONSTRAINT  customerPk  PRIMARY KEY(customerID)
 );
 
 CREATE TABLE Worker (
     workerID   Integer    NOT NULL,
-    userName   Char(25)   NOT NULL,
+    username   Char(25)   NOT NULL,
     password   Char(25)   NOT NULL,
     prefwifidata   Integer  DEFAULT 1,
     prefbattery    Integer  DEFAULT 1,
     CONSTRAINT  workerPk  PRIMARY KEY(workerID),
-    CONSTRAINT  projectFk FOREIGN KEY(projectID)
+    CONSTRAINT  projectFk FOREIGN KEY(pID)
+        REFERENCES Project(pID),
 );
 
 CREATE TABLE Project (
-    projectID   Integer    NOT NULL,
-    projectName Char(25)   NOT NULL,
-    status      Char(25)   DEFAULT 'NOT COMPLETE',
+    pID   Integer    NOT NULL,
+    pname Char(25)   NOT NULL,
+    pdescription,
     customerID  Integer    NOT NULL,
-    CONSTRAINT  projectPk  PRIMARY KEY(projectID),
+    CONSTRAINT  projectPk  PRIMARY KEY(pID),
     CONSTRAINT  customerFk FOREIGN KEY(customerID)
         REFERENCES Customer(customerID) ON DELETE CASCADE,
 );
 
 CREATE TABLE Project_task (
     taskID      Integer    NOT NULL,
-    controlData DATA,
-    status      Char(25)   DEFAULT 'NOT COMPLETE',
-    resultData  DATA,
-    projectID   Integer    NOT NULL,
+    task,
+    pID   Integer    NOT NULL,
     workerID    Integer,
     CONSTRAINT  taskPk  PRIMARY KEY(taskID),
-    CONSTRAINT  projectFk FOREIGN KEY(projectID)
-        REFERENCES Project(projectID) ON DELETE CASCADE,
+    CONSTRAINT  projectFk FOREIGN KEY(pID)
+        REFERENCES Project(pID) ON DELETE CASCADE,
     CONSTRAINT  workerFk FOREIGN KEY(workerID)
         REFERENCES Worker(workerID) ON DELETE CASCADE,
 );
 
 CREATE TABLE Data_blob (
     blobID      Integer    NOT NULL,
-    blobData    BLOB,
-    projectID   Integer    NOT NULL,
+    blob,
+    metadata,
+    pID   Integer    NOT NULL,
     CONSTRAINT  blobPk  PRIMARY KEY(blobID),
-    CONSTRAINT  projectFk FOREIGN KEY(projectID)
-        REFERENCES Project(projectID) ON DELETE CASCADE,
+    CONSTRAINT  projectFk FOREIGN KEY(pID)
+        REFERENCES Project(pID) ON DELETE CASCADE,
 );
 
 CREATE TABLE Blob_task_entity (
@@ -55,4 +55,14 @@ CREATE TABLE Blob_task_entity (
         REFERENCES Data_blob(blobID) ON DELETE CASCADE,
     CONSTRAINT  taskFk FOREIGN KEY(taskID)
         REFERENCES Project_task(taskID) ON DELETE CASCADE,
+);
+
+CREATE TABLE Completed_task (
+    pID      Integer    NOT NULL,
+    taskID      Integer    NOT NULL,
+    blobs,
+    blobmetadatas,
+    CONSTRAINT  taskIDPk  PRIMARY KEY(taskID),
+    CONSTRAINT  pIDFk FOREIGN KEY(pID)
+        REFERENCES Project(pID) ON DELETE CASCADE
 );
