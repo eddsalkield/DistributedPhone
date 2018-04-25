7b953036a5d9a65d98d6727b435315e854a308e7
OliverWD <derektherock.rip@gmail.com>
2018-04-25 18:23:32 +0100

    Update collatzClient.c

--- testController/collatzClient.c
+++ testController/collatzClient.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
+typedef unsigned int seqlen_t;
+
 inline __uint128_t nextStep (__uint128_t n) {
     if (n % 2 == 0)
         return n / 2;
@@ -11,39 +13,53 @@ inline __uint128_t nextStep (__uint128_t n) {
     return 0;
 }
 
+__uint128_t u128_read(const void *vin) {
+	const unsigned char *in = (const unsigned char*)vin;
+	__uint128_t val = 0;
+	for (int i = 0; i < 16; i++) {
+	    val += (__uint128_t)in[i] << 8*i;
+	}
+	return val;
+}
+
+void seqlen_write(const void *vout, seqlen_t val) {
+    unsigned char *out = (unsigned char*)vout;
+    int s = sizeof(seqlen_t);
+    for (int i = 0; i < s; i++) {
+	out[i] = (val >> 8*i);
+    }
+}
+
+
 // Runs a task. The request's blobs, as well as the request object itself,
 // have been allocated using pptw1_malloc().
 
 // Input on control, Output in blob
 
 struct pptw1_response *pptw1_run(struct pptw1_request* req) {
-
+    
     // Return response
-    struct pptw1_response * response = malloc(sizeof(struct pptw1_response) + n_blobs * sizeof(struct pptw1_blobref));
+    struct pptw1_response * response = malloc(sizeof(struct pptw1_response) + sizeof(struct pptw1_blobref));
 
     // assert blobs == 0 (input in control)
     assert(req->n_blobs == 0);
-    response->n_blobs = 1; // Output a single blob
 
-    // Extract blob data from request
+    // Output a single blob
+    response->n_blobs = 1; 
+
+    // Extract blob data from request 
     struct pptw1_blobref reqControlBlob = req->control;
-    __uint128_t *data = (__uint128_t *)(reqControlBlob.data); 
+    assert(reqControlBlob.size == 2 * sizeof(__uint128_t));
+    __uint128_t *data = (__uint128_t *)(reqControlBlob.data);
     __uint128_t left = data[0];
     __uint128_t right = data[1];
-    __uint128_t range = right - left;
 
     // Our blob memory
-    char* blobMemChar = pptw1_malloc(sizeof(unsigned int[range]));
-
-    // Cast to unsigned shorts -- sequence lengths are quite short
-    unsigned int *blobMem = (unsigned int *)(blobMemChar);
-
-    // This represents how many sequences have been computed this blob
-    // assume |range| < 2 mil (else a lot of memory per blob)
-    int seqsDone = 0;
+    char* blobMemStart = pptw1_malloc(sizeof(seqlen_t[right - left]));
+    char* blobMemIndex = blobMemStart;
 
     __uint128_t num;  // current number we are testing
-    unsigned int seqLength; // length of that sequence
+    seqlen_t seqLength; // length of that sequence
 
     for (__uint128_t i = left; i < right; i++) {
 	num = i; seqLength = 0;
@@ -54,15 +70,15 @@ struct pptw1_response *pptw1_run(struct pptw1_request* req) {
 	}
 	// may have overflowed
 	if (num == 1) {
-	    blobMem[seqsDone] = seqLength;
-            seqsDone++;
+	    seqlen_write(blobMemIndex, seqLength);
+	    blobMemIndex += sizeof(seqlen_t);
         }
     }    
         
     // Info has been written to memory[blobMem ... blobMem + seqDone)
     struct pptw1_blobref * thisBlob = &(response->blobs[0]); // we send a single blob
-    thisBlob->data = blobMem;
-    thisBlob->size = sizeof(unsigned int) * seqsDone;
+    thisBlob->data = blobMemStart;
+    thisBlob->size = blobMemIndex - blobMemStart; 
     
     return response;
 }
