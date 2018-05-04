a881fcf47cb79ba221614fad5814fde8f0ca8e76
Hristo Venev <hristo@venev.name>
2018-05-04 23:17:47 +0100

    Implement basic UI controller.
    Settings are not saved yet, but the login state (mostly) is.

--- client/src/controller.ts
+++ client/src/controller.ts
@@ -1,5 +1,7 @@
 import * as exec_api from "@/exec/api";
 
+import * as ui_api from "@/API";
+
 import * as cbor from "@/cbor";
 import * as cbut from "@/cbor-util";
 import * as err from "@/err";
@@ -8,12 +10,7 @@ import * as stat from "@/stat";
 import IDBStorage from "@/exec/idb_storage";
 import Runner from "@/exec/runner";
 
-interface ReqOpts {
-    data?: Uint8Array;
-    expect_size?: number;
-}
-
-export class TSDB implements stat.TSDB {
+class TSDB implements stat.TSDB {
     constructor() {}
 
     public write(at: number, data: stat.Point[]): void {
@@ -25,30 +22,139 @@ export class TSDB implements stat.TSDB {
     }
 }
 
-export default class Controller {
+interface ReqOpts {
+    data?: Uint8Array;
+    expect_size?: number;
+}
+
+interface UserData {
+    readonly name: string;
+    readonly token: string;
+}
+
+interface Settings {
+    project: string | null;
+    my_projects: string[];
+}
+
+function parseResponse(data: ArrayBuffer, handlers: {[name:string]: ((r: cbor.Reader) => void) | undefined}): void {
+    let success: boolean | undefined;
+    let error: string | undefined;
+
+    try {
+        const r = new cbor.Reader(data);
+        r.map();
+        while(true) {
+            const key = r.maybeString();
+            if(key === null) break;
+            if(key === "success") success = r.boolean();
+            else if(key === "error") error = r.string();
+            else {
+                const h = handlers[key];
+                if(h === undefined) r.skip();
+                else h(r);
+            }
+        }
+        r.end();
+    } catch(e) {
+        if(e instanceof TypeError) throw new err.Validation(e.message);
+        throw e;
+    }
+
+    if(!success) {
+        throw new err.Runtime(error || "Unknown server error");
+    }
+}
+
+export class Controller {
     public readonly stat_root: stat.Root;
 
-    private token?: Promise<string>;
-    private on_login?: (token: string) => void;
-    private runner: Promise<Runner>;
+    public user: UserData | null = null;
+    public user_promise!: Promise<UserData>;
+    private on_login?: (data: UserData) => void;
+
+    public runner_promise!: Promise<Runner>;
     private on_runner?: (runner: Promise<Runner>) => void;
-    private project: string = "";
+
+    private settings_update_promise!: Promise<void>;
+    private on_settings_update!: () => void;
+
+    private cfg: Settings = {
+        project: null,
+        my_projects: [],
+    };
 
     constructor(
         /* Base URL with trailing slash */
         public readonly url: string,
     ) {
         this.stat_root = new stat.Root(new TSDB());
-        this.runner = this.runnerPromise();
+        this.newUserPromise();
+        this.newRunnerPromise();
+        this.newSettingsUpdatePromise();
+
+        const ls = window.localStorage;
+        if(ls.login_data !== undefined) {
+            this.on_login!(JSON.parse(ls.login_data));
+        }
     }
 
-    private runnerPromise(): Promise<Runner> {
-        return new Promise((resolve, reject) => {
-            this.on_runner = (r) => {
-                delete this.on_runner;
-                resolve(r);
+    private newUserPromise(): void {
+        this.user_promise = new Promise((resolve, reject) => {
+            this.on_login = (u) => {
+                this.user = u;
+                window.localStorage.login_data = JSON.stringify(u);
+                delete this.on_login;
+                resolve(u);
             };
         });
+    }
+
+    private newRunnerPromise(): void {
+        this.runner_promise = new Promise((resolve, reject) => {
+            this.on_runner = (r) => {
+                delete this.on_runner;
+                r.then((v) => {
+                    resolve(v);
+                }, reject);
+            };
+        });
+    }
+
+    private newSettingsUpdatePromise(): void {
+        this.settings_update_promise = new Promise((resolve, reject) => {
+            this.on_settings_update = () => {
+                this.newSettingsUpdatePromise();
+                resolve();
+            };
+        });
+    }
+
+    public onSettingsUpdate<T>(f: () => Promise<T>): Promise<T> {
+        return this.settings_update_promise.then(f);
+    }
+
+    public settingsUpdated(): void {
+        this.on_settings_update();
+    }
+
+    public getProject(): string | null {
+        return this.cfg.project;
+    }
+
+    public setProject(p: string | null): void {
+        if(this.cfg.project === p) return;
+        this.cfg.project = p;
+        this.settingsUpdated();
+    }
+
+    public getMyProjects(): string[] {
+        return Array.from(this.cfg.my_projects);
+    }
+
+    public setMyProjects(p: string[]): void {
+        this.cfg.my_projects = Array.from(p);
+        this.settingsUpdated();
     }
 
     public startExec(): void {
@@ -58,25 +164,26 @@ export default class Controller {
         }));
     }
 
+    public withUser<T>(f: (login: UserData) => Promise<T>): Promise<T> {
+        return this.user_promise.then(f);
+    }
+
     public withToken<T>(f: (token: string) => Promise<T>): Promise<T> {
-        if(this.token === undefined) {
-            this.token = new Promise((resolve, reject) => {
-                this.on_login = resolve;
-            });
-        }
-        return this.token.then(f);
+        return this.withUser((l) => {
+            return f(l.token);
+        });
     }
 
     public withRunner<T>(f: (r: Runner) => Promise<T>): Promise<T> {
-        return this.runner.then(f);
+        return this.runner_promise.then(f);
     }
 
     public resetExec(): Promise<void> {
         if(this.on_runner !== undefined) {
             return IDBStorage.delete("blob_storage");
         }
-        return this.runner.then((r) => {
-            this.runner = this.runnerPromise();
+        return this.runner_promise.then((r) => {
+            this.newRunnerPromise();
             return r.stop();
         }).catch(() => {}).then(() => {
             return IDBStorage.delete("blob_storage");
@@ -97,51 +204,46 @@ export default class Controller {
         return this.request("login", {
             data: req.done(),
         }).then((data) => {
-            let success: boolean | undefined;
-            let error: string | undefined;
             let tok: string | undefined;
 
-            try {
-                const r = new cbor.Reader(data);
-                r.map();
-                while(true) {
-                    const key = r.maybeString();
-                    if(key === null) break;
-                    if(key === "success") success = r.boolean();
-                    else if(key === "error") error = r.string();
-                    else if(key === "token") tok = r.string();
-                    else r.skip();
-                }
-                r.end();
-            } catch(e) {
-                if(e instanceof TypeError) throw new err.Validation(e.message);
-                throw e;
-            }
-
-            if(!success) {
-                throw new err.Runtime(error || "Unknown server error");
-            }
+            parseResponse(data, {
+                "token": (r) => {
+                    tok = r.string();
+                },
+            });
 
             if(tok === undefined) {
                 throw new err.Validation("Server provided no token");
             }
 
-            const f = this.on_login;
-            if(f === undefined) {
-                this.token = Promise.resolve(tok);
-            } else {
-                delete this.on_login;
-                f(tok);
+            if(this.on_login === undefined) {
+                throw new err.State("Already logged in");
             }
+
+            this.on_login({
+                name: username,
+                token: tok,
+            });
         });
     }
 
-    public setProject(project: string) {
-        this.project = project;
-    }
+    public logout(): void {
+        const ud = this.user;
+        if(ud === null) return;
 
-    public getProject(): string {
-        return this.project;
+        delete window.localStorage.login_data;
+        this.user = null;
+        this.newUserPromise();
+
+        const req = new cbor.Writer();
+        req.map(1);
+        req.string("token");
+        req.string(ud.token);
+        req.end();
+
+        this.request("logout", {
+            data: req.done(),
+        });
     }
 
     public request(path: string, opts: ReqOpts): Promise<ArrayBuffer> {
@@ -261,30 +363,12 @@ class WorkProvider implements exec_api.WorkProvider {
                 expect_size: expected_size,
             });
         }).then((data) => {
-            let success: boolean | undefined;
-            let error: string | undefined;
             let blob: Uint8Array | undefined;
-
-            try {
-                const r = new cbor.Reader(data);
-                r.map();
-                while(true) {
-                    const key = r.maybeString();
-                    if(key === null) break;
-                    else if(key === "success") success = r.boolean();
-                    else if(key === "error") error = r.string();
-                    else if(key === "blob") blob = r.bytes();
-                    else r.skip();
-                }
-                r.end();
-            } catch(e) {
-                if(e instanceof TypeError) throw new err.Validation(e.message);
-                throw e;
-            }
-
-            if(!success) {
-                throw new err.Runtime(error || "Unknown server error");
-            }
+            parseResponse(data, {
+                "blob": (r) => {
+                    blob = r.bytes();
+                },
+            });
 
             if(blob === undefined) {
                 throw new err.Validation("No blob provided");
@@ -296,6 +380,11 @@ class WorkProvider implements exec_api.WorkProvider {
 
     public getTasks(): Promise<exec_api.TaskSet> {
         const project = this.ctl.getProject();
+        if(project === null) {
+            return this.ctl.onSettingsUpdate(() => {
+                return this.getTasks();
+            });
+        }
 
         const req = new cbor.Writer();
         req.map(3);
@@ -313,32 +402,16 @@ class WorkProvider implements exec_api.WorkProvider {
                 data: req.done(),
             });
         }).then((data): exec_api.TaskSet | Promise<exec_api.TaskSet> => {
-            let success: boolean | undefined;
-            let error: string | undefined;
             let taskIDs: string[] | undefined;
             let taskBlobs: Uint8Array[] | undefined;
-
-            try {
-                const r = new cbor.Reader(data);
-                r.map();
-                while(true) {
-                    const key = r.maybeString();
-                    if(key === null) break;
-                    if(key === "success") success = r.boolean();
-                    else if(key === "error") error = r.string();
-                    else if(key === "taskIDs") taskIDs = cbut.readArray(r, (rd) => rd.string());
-                    else if(key === "tasks") taskBlobs = cbut.readArray(r, (rd) => rd.bytes());
-                    else r.skip();
-                }
-                r.end();
-            } catch(e) {
-                if(e instanceof TypeError) throw new err.Validation(e.message);
-                throw e;
-            }
-
-            if(!success) {
-                throw new err.Runtime(error || "Unknown server error");
-            }
+            parseResponse(data, {
+                "taskIDs": (r) => {
+                    taskIDs = cbut.readArray(r, (rd) => rd.string());
+                },
+                "tasks": (r) => {
+                    taskBlobs = cbut.readArray(r, (rd) => rd.bytes());
+                },
+            });
 
             if(taskIDs === undefined) throw new err.Validation("taskIDs");
             if(taskBlobs === undefined) throw new err.Validation("tasks missing");
@@ -423,28 +496,171 @@ class WorkProvider implements exec_api.WorkProvider {
                 data: req.done(),
             });
         }).then((data) => {
-            let success: boolean | undefined;
-            let error: string | undefined;
+            parseResponse(data, {});
+        });
+    }
+}
 
-            try {
-                const r = new cbor.Reader(data);
-                r.map();
-                while(true) {
-                    const key = r.maybeString();
-                    if(key === null) break;
-                    if(key === "success") success = r.boolean();
-                    else if(key === "error") error = r.string();
-                    else r.skip();
-                }
-                r.end();
-            } catch(e) {
-                if(e instanceof TypeError) throw new err.Validation(e.message);
+type ProjectMap = Map<string, ui_api.Project>;
+
+export class UIState implements ui_api.ClientStateInterface {
+    constructor(public readonly ctl: Controller) {}
+
+    // TODO: implement
+    public get ChargingOnly() {
+        return false;
+    }
+    public get AllowDataUsage() {
+        return true; // this.ctl.cfg.allow_mobile_data;
+    }
+    // TODO: decode name
+    public get NewProjectMyProjectsID() {
+        return "Fibonacci counter";
+    }
+
+
+    public get ProjectChoiceID() {
+        return this.ctl.getProject();
+    }
+    public get IsLoggedIn() {
+        return this.ctl.user !== null;
+    }
+
+    public login(username: string, password: string): Promise<void> {
+        return this.ctl.login(username, password);
+    }
+    public loginGuest(): Promise<void> {
+        return Promise.reject(new err.Runtime("Not implemented"));
+    }
+    public logout(): Promise<void> {
+        try {
+            this.ctl.logout();
+            return Promise.resolve();
+        } catch(e) {
+            return Promise.reject(e);
+        }
+    }
+
+    public SignUp(username: string, password:string): Promise<void> {
+        const req = new cbor.Writer();
+        req.map(3);
+        req.string("username");
+        req.string(username);
+        req.string("password");
+        req.string(password);
+        req.string("accesslevel");
+        req.string("worker");
+        req.end();
+
+        return this.ctl.request("register", {
+            data: req.done(),
+        }).then((data) => {
+            parseResponse(data, {});
+            // Hope login succeeds.
+            return this.login(username, password);
+        });
+    }
+
+    private requestProjects(): Promise<ProjectMap> {
+        const ud = this.ctl.user;
+        if(ud === null) return Promise.reject(new err.State("Not logged in"));
+
+        const req = new cbor.Writer();
+        req.map(1);
+        req.string("token");
+        req.string(ud.token);
+        req.end();
+
+        return this.ctl.request("getProjectsList", {
+            data: req.done(),
+        }).then((data) => {
+            const ps: ProjectMap = new Map();
+            parseResponse(data, {
+                "projects": (r) => {
+                    r.map();
+                    while(true) {
+                        const name = r.maybeString();
+                        if(name === null) break;
+                        let desc: string = name;
+                        r.map();
+                        while(true) {
+                            const key = r.maybeString();
+                            if(key === null) break;
+                            if(key === "description") desc = r.string();
+                            else r.skip();
+                        }
+                        r.end();
+
+                        ps.set(name, {
+                            Title: name,
+                            Description: desc,
+                        });
+                    }
+                    r.end();
+                },
+            });
+
+            return ps;
+        });
+    }
+
+    private cached_projects?: ProjectMap;
+    private cached_projects_promise?: Promise<ProjectMap>;
+    private cached_projects_expiry: number = 0; // timestamp in milliseconds
+    // If not refresh, try returning soon (1 second if a request is already
+    // happening, immediately otherwise). If refresh, send a request unless one
+    // is pending.
+    private getProjects(refresh: boolean): Promise<ProjectMap> {
+        let pr = this.cached_projects_promise;
+        let cached = this.cached_projects;
+
+        if(cached !== undefined) {
+            const now = new Date().getTime();
+            if(now > this.cached_projects_expiry) {
+                cached = undefined;
+                delete this.cached_projects;
+            }
+        }
+
+        if(pr === undefined) {
+            if(!refresh && cached !== undefined) return Promise.resolve(cached);
+
+            pr = this.requestProjects().then((pmap) => {
+                delete this.cached_projects_promise;
+                this.cached_projects = pmap;
+                this.cached_projects_expiry = new Date().getTime() + 3600 * 1000;
+                return pmap;
+            }, (e) => {
+                delete this.cached_projects_promise;
                 throw e;
-            }
+            });
 
-            if(!success) {
-                throw new err.Runtime(error || "Unknown server error");
+            this.cached_projects_promise = pr;
+        }
+
+        if(!refresh && cached === undefined) return pr;
+
+        return Promise.race([pr, new Promise<ProjectMap>((resolve, reject) => {
+            setTimeout(() => {
+                resolve(cached);
+            }, 1000);
+        })]);
+    }
+
+    public ListOfAllProjects(): Promise<ui_api.Project[]> {
+        return this.getProjects(true).then((ps) => {
+            return Array.from(ps.values());
+        });
+    }
+
+    public ListOfMyProjects(): Promise<ui_api.Project[]> {
+        return this.getProjects(false).then((pmap) => {
+            const ps: ui_api.Project[] = [];
+            for(const pid of this.ctl.getMyProjects()) {
+                const p = pmap.get(pid);
+                if(p !== undefined) ps.push(p);
             }
+            return ps;
         });
     }
 }
