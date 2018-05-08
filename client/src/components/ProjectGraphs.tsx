e2571a28333bc2d543e5e0f98cffde64ba01ae12
Hristo Venev <hristo@venev.name>
2018-05-08 13:45:16 +0100

    Show project name and description in graphs.

--- client/src/components/ProjectGraphs.tsx
+++ client/src/components/ProjectGraphs.tsx
@@ -4,6 +4,7 @@ import * as obs from "@/obs";
 
 import * as api from "../API";
 
+import Loading from "./Loading";
 import {Chart, ChartConfiguration} from "./Chart";
 
 import "./ProjectGraphs.css";
@@ -16,6 +17,7 @@ interface Props {
 interface State {
     serial: number;
     error: string | null;
+    project_data?: api.Project | null;
 }
 
 function flatLine(data: api.GraphPoint[]): api.GraphPoint[] {
@@ -118,6 +120,12 @@ export default class ProjectGraphs extends React.Component<Props, State> {
 
     private reset() {
         for(const sub of this.subs) sub.stop();
+
+        this.setState({
+            error: null,
+            project_data: undefined,
+        });
+
         this.subs = [
             obs.refresh(() => {
                 return this.props.user.requestGraphs(
@@ -133,10 +141,14 @@ export default class ProjectGraphs extends React.Component<Props, State> {
                 this.chart2.data!.datasets![1].data = flatLine(graphs.tasksRefused);
                 this.chart2.data!.datasets![2].data = flatLine(graphs.tasksFailed);
 
-                this.setState((st) => ({serial: st.serial+1}));
+                this.setState((st) => ({serial: st.serial + 1}));
             }, (e) => {
                 this.setState({error: e.message});
                 // Handle error here
+            }),
+            this.props.user.projects.subscribe((p) => {
+                const pd = p.get(this.props.project);
+                this.setState({project_data: pd === undefined ? null : pd});
             }),
         ];
         for(const sub of this.subs) sub.start();
@@ -150,10 +162,31 @@ export default class ProjectGraphs extends React.Component<Props, State> {
         for(const sub of this.subs) sub.stop();
     }
 
+    public componentDidUpdate(prev_props: Props) {
+        const props = this.props;
+        if(
+            props.user !== prev_props.user ||
+            props.project !== prev_props.project
+        ) {
+            this.reset();
+        }
+    }
+
     public render() {
-        const {serial, error} = this.state;
+        const {serial, error, project_data} = this.state;
+        if(project_data === undefined) {
+            return <Loading />;
+        }
+
+        if(project_data === null) {
+            return <div className="Main ProjectGraphs">
+                <span className="ProjectGraphs-error">Project not found</span>
+            </div>;
+        }
 
         return <div className="Main ProjectGraphs">
+            <h2>{project_data.title}</h2>
+            <p>{project_data.description}</p>
             {error === null ? [
                 <Chart options={this.chart1} serial={serial} />,
                 <Chart options={this.chart2} serial={serial} />,
